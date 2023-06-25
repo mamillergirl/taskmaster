1333ffc51e051ab18f59318aa53d5e676fbb3e58
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-06-24 20:37:30 -0700

    update other routes to show up on api-docs and work with db

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -1,47 +1,45 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
-// const passwordUtil = require('../util/passwordComplexityCheck');
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
-const getAll = (req, res) => {
-    mongodb
-        .getDb()
-        .db("taskmaster")
-        .collection('inventory')
-        .find()
-        .toArray((err, lists) => {
-            if (err) {
-                res.status(400).json({ message: err });
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists);
-        });
+const getAll =  (req, res) => {
+  mongodb.getDb()
+  .db("taskmaster")
+  .collection("inventory")
+  .find()
+  .toArray((err) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    }
+  })
+  .then((lists) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(lists);
+  });
 };
 
-const getSingle = (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid inventory id to find a inventory.');
-    }
-    const inventoryId = new ObjectId(req.params.id);
-    mongodb
-        .getDb()
-        .db("taskmaster")
-        .collection('inventory')
-        .find({ _id: inventoryId })
-        .toArray((err, result) => {
-            if (err) {
-                res.status(400).json({ message: err });
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(result[0]);
-        });
+const getSingle = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid inventory id to find a inventory.');
+  }
+  const inventoryId = new ObjectId(req.params.id);
+  mongodb
+    .getDb()
+    .db("taskmaster")
+    .collection("inventory")
+    .find({ _id: inventoryId })
+    .toArray((err) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+    })
+    .then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists[0]);
+    });
+
 };
 
 const createInventory = async (req, res) => {
-    if (!req.body.username || !req.body.password) {
-        res.status(400).send({ message: 'Content can not be empty!' });
-        return;
-      }
-    
     const inventory = {
         productCategory: req.body.productCategory,
         productName: req.body.productName,
@@ -57,74 +55,66 @@ const createInventory = async (req, res) => {
         dateCreation: req.body.dateCreation
     };
 
-    const response = await mongodb.getDb().db("taskmaster").collection('inventory').insertOne(inventory);
-    if (response.acknowledged) {
-        res.status(201).json(response);
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating the inventory.');
-    }
+  const response = await mongodb
+    .getDb()
+    .db("taskmaster")
+    .collection("inventory")
+    .insertOne(inventory)
+  if (response.acknowledged){
+    res.status(201).json(response);
+  }
+  else {
+    res.status(500).json(response.error);
+  }
 };
 
 const updateInventory = async (req, res) => {
-    const inventoryId = new ObjectId(req.params.id);
-    // be aware of updateOne if you only want to update specific fields
-    const inventory = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        phoneNumber: req.body.phoneNumber,
-        birthday: req.body.birthday,
-        address: req.body.address,
-        interest: req.body.interest,
-        occupation: {
-            jobTitle: req.body.jobTitle,
-            responsibilities: req.body.responsibilities,
-            education: req.body.education,
-            expertise: req.body.expertise,
-            skills: req.body.skills,
-            salary: req.body.salary
-        }, 
-        emergencyContact: {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            phoneNumber: req.body.phoneNumber,
-            email: req.body.email,
-            address: req.body.address
-        }
-    };
-    const response = await mongodb
-        .getDb()
-        .db("taskmaster")
-        .collection('inventory')
-        .replaceOne({ _id: inventoryId }, inventory);
-    console.log(response);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the inventory.');
-    }
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid inventory id to find a inventory.');
+  }
+  const inventory = {
+    productCategory: req.body.productCategory,
+    productName: req.body.productName,
+    productDescription: req.body.productDescription,
+    brand: req.body.brand,
+    countryOrigin: req.body.countryOrigin,
+    productWidth: req.body.productWidth,
+    productHeight: req.body.productHeight,
+    productVolume: req.body.productVolume,
+    productWeight: req.body.productWeight,
+    packType: req.body.packType,
+    expiryDate: req.body.expiryDate,
+    dateCreation: req.body.dateCreation
 };
 
-const deleteInventory = async (req, res) => {
-    const inventoryId = new ObjectId(req.params.id);
-    const response = await mongodb
+  const inventoryId = new ObjectId(req.params.id);
+  const response = await mongodb
     .getDb()
     .db("taskmaster")
-    .collection('inventory')
-    .remove({ _id: inventoryId }, true);
-    console.log(response);
-
-    if (response.deletedCount > 0) {
-        res.status(204).send();
+    .collection("inventory")
+    .replaceOne({ _id: inventoryId }, inventory);
+    
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the inventory.');
+      res.status(500).json(response.error || 'Unable to update inventory.');
     }
-};
-
-module.exports = {
-    getAll,
-    getSingle,
-    createInventory,
-    updateInventory,
-    deleteInventory
-};
+  }
+  const deleteInventory = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid inventory id to find a inventory.');
+    }
+    const inventoryId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDb()
+      .db("taskmaster")
+      .collection("inventory")
+      .deleteOne( { "_id" : inventoryId} );
+      if (response.deletedCount > 0) {
+        res.status(200).send();
+      } else {
+        res.status(500).json(response.error || 'Unable to delete inventory.');
+      }
+  };
+  
+module.exports = { getAll, getSingle, createInventory, updateInventory, deleteInventory};
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,106 +1,108 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
-// const passwordUtil = require('../util/passwordComplexityCheck');
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
-const getAll = (req, res) => {
-    mongodb
-        .getDb()
-        .db("taskmaster")
-        .collection('task')
-        .find()
-        .toArray((err, lists) => {
-            if (err) {
-                res.status(400).json({ message: err });
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists);
-        });
+const getAll =  (req, res) => {
+  mongodb.getDb()
+  .db("taskmaster")
+  .collection("task")
+  .find()
+  .toArray((err) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    }
+  })
+  .then((lists) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(lists);
+  });
 };
 
-const getSingle = (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid task id to find a task.');
-    }
-    const taskId = new ObjectId(req.params.id);
-    mongodb
-        .getDb()
-        .db("taskmaster")
-        .collection('task')
-        .find({ _id: taskId })
-        .toArray((err, result) => {
-            if (err) {
-                res.status(400).json({ message: err });
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(result[0]);
-        });
+const getSingle = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid task id to find a task.');
+  }
+  const taskId = new ObjectId(req.params.id);
+  mongodb
+    .getDb()
+    .db("taskmaster")
+    .collection("task")
+    .find({ _id: taskId })
+    .toArray((err) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+    })
+    .then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists[0]);
+    });
+
 };
 
 const createTask = async (req, res) => {
-    
     const task = {
         taskName: req.body.taskName,
         taskInitiator: req.body.taskInitiator,
-        taskStartdate: req.body.taskStartdate,
+        taskStartDate: req.body.taskStartDate,
         responsiblePerson: req.body.responsiblePerson,
         taskDeadline: req.body.taskDeadline,
         taskOutcome: req.body.taskOutcome,
-        assignedTeam: req.body.assignedTeam
+        assignedtask: req.body.assignedtask
     };
-
-    const response = await mongodb.getDb().db("taskmaster").collection('task').insertOne(task);
-    if (response.acknowledged) {
-        res.status(201).json(response);
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating the task.');
-    }
+  const response = await mongodb
+    .getDb()
+    .db("taskmaster")
+    .collection("task")
+    .insertOne(task)
+  if (response.acknowledged){
+    res.status(201).json(response);
+  }
+  else {
+    res.status(500).json(response.error);
+  }
 };
 
 const updateTask = async (req, res) => {
-    const taskId = new ObjectId(req.params.id);
-    // be aware of updateOne if you only want to update specific fields
-    const task = {
-        taskName: req.body.taskName,
-        taskInitiator: req.body.taskInitiator,
-        taskStartdate: req.body.taskStartdate,
-        responsiblePerson: req.body.responsiblePerson,
-        taskDeadline: req.body.taskDeadline,
-        taskOutcome: req.body.taskOutcome,
-        assignedTeam: req.body.assignedTeam
-    };
-    const response = await mongodb
-        .getDb()
-        .db("taskmaster")
-        .collection('task')
-        .replaceOne({ _id: taskId }, task);
-    console.log(response);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the task.');
-    }
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid task id to find a task.');
+  }
+  const task = {
+    taskName: req.body.taskName,
+    taskInitiator: req.body.taskInitiator,
+    taskStartDate: req.body.taskStartDate,
+    responsiblePerson: req.body.responsiblePerson,
+    taskDeadline: req.body.taskDeadline,
+    taskOutcome: req.body.taskOutcome,
+    assignedtask: req.body.assignedtask
 };
-
-const deleteTask = async (req, res) => {
-    const taskId = new ObjectId(req.params.id);
-    const response = await mongodb
+  const taskId = new ObjectId(req.params.id);
+  const response = await mongodb
     .getDb()
     .db("taskmaster")
-    .collection('task')
-    .remove({ _id: taskId }, true);
-    console.log(response);
-
-    if (response.deletedCount > 0) {
-        res.status(204).send();
+    .collection("task")
+    .replaceOne({ _id: taskId }, task);
+    
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the task.');
+      res.status(500).json(response.error || 'Unable to update task.');
     }
-};
-
-module.exports = {
-    getAll,
-    getSingle,
-    createTask,
-    updateTask,
-    deleteTask
-};
+  }
+  const deleteTask = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid task id to find a task.');
+    }
+    const taskId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDb()
+      .db("taskmaster")
+      .collection("task")
+      .deleteOne( { "_id" : taskId} );
+      if (response.deletedCount > 0) {
+        res.status(200).send();
+      } else {
+        res.status(500).json(response.error || 'Unable to delete task.');
+      }
+  };
+  
+module.exports = { getAll, getSingle, createTask, updateTask, deleteTask };
